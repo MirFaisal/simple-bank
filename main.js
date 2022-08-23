0f91754c3ab92aab411b8589d099b595223d8c68
Mir Fasial <mirfaisal08@gmail.com>
2022-08-23 10:37:36 +0600

    set login function

---
+++ main.js
@@ -0,0 +1,14 @@
+const loginBtn = document.getElementById("btn-login");
+loginBtn.addEventListener("click", function () {
+    const emailFild = document.getElementById("floatingInput");
+    const passfild = document.getElementById("floatingPassword");
+    const password = passfild.value;
+    const email = emailFild.value;
+    if (email === 'mirfaisal08@gmail.com' && password === 'onektaka') {
+        location.replace("bank.html")
+    }
+    else {
+        alert("Please check your Email address and Password")
+    }
+    //console.log(email);
+})
